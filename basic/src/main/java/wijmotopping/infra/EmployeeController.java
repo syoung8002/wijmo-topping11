850e563f4b60f1209b3bc973b5934c639ef134ab
syoung8002 <48265118+syoung8002@users.noreply.github.com>
2023-05-11 13:06:23 +0900

    commit

--- basic/src/main/java/wijmotopping/infra/EmployeeController.java
+++ basic/src/main/java/wijmotopping/infra/EmployeeController.java
@@ -1,20 +1,29 @@
 package wijmotopping.infra;
+import wijmotopping.domain.*;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import wijmotopping.domain.*;
+
 
 @RestController
 // @RequestMapping(value="/employees")
 @Transactional
 public class EmployeeController {
-
     @Autowired
     EmployeeRepository employeeRepository;
+
+
+
+
+    @PutMapping("/employees/search/")
+    public Object (@RequestBody EmployeeViewQuery query){
+        return employeeRepository.(query.UserId(),query.UserName(),query.Department());
+    }
 }
